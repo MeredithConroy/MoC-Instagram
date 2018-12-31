612b8c582bb4ffdc25990f19b558acf5296f3cbc
Meredith Conroy <mere.conroy@gmail.com>
2018-12-31 12:14:50 -0800

    removing duplicate code - script for extracting instagram handles

--- socialMoC.R
+++ socialMoC.R
@@ -2,26 +2,6 @@ library('yaml')
 
 social <- yaml.load_file("legislators-social-media.yaml.txt")
 
-for(soc in social) {
-  s <- sprintf("%s: %s", soc$id$bioguide, soc$social$instagram)
-  print(s)
-}
-library('yaml')
-
-social <- yaml.load_file("legislators-social-media.yaml.txt")
-
-for(soc in social) {
-  if(length(soc$social$instagram) > 0) {
-    s <- sprintf("%s: %s", soc$id$bioguide, soc$social$instagram)
-    print(s)
-  } else {
-    print(soc$id$bioguide)
-  }
-}
-library('yaml')
-
-social <- yaml.load_file("legislators-social-media.yaml.txt")
-
 bio <- yaml.load_file("legislators-current.yaml.txt")
 
 for(soc in social) {
